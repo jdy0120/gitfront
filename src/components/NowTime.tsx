11cbe71df0407276159df09a1b9789f43ccc6246
doyeon <jdy0210@naver.com>
2021-03-24 18:55:26 +0900

    add time components

---
+++ src/components/NowTime.tsx
@@ -0,0 +1,34 @@
+import react, { useState } from 'react';
+
+type time = {
+  hour: number,
+  minute: number,
+  seconds: number
+}
+
+const NowTime = () => {
+  const [nowTime,setNowTime] = useState<time>({
+    hour: 0,
+    minute: 0,
+    seconds: 0
+  });
+
+  const setTime = () => {
+    const date = new Date();
+    setNowTime({
+      hour: date.getHours(),
+      minute: date.getMinutes(),
+      seconds: date.getSeconds()
+    });
+  }
+  setInterval(setTime,1000);
+
+  return (
+    <div>
+      <p>현재 시간은</p>
+      <p>{nowTime.hour}시 {nowTime.minute}분 {nowTime.seconds}초 입니다.</p>
+      <p>하지만 도연이의 퇴근 시간은 10시 0분 0초 입니다.</p>
+    </div>
+  );
+}
+export default NowTime;
