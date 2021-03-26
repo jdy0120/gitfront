c008578b22d81047702b1158ca80a4a54f7120fe
doyeon <jdy0210@naver.com>
2021-03-26 21:03:57 +0900

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
+import { Route, BrowserRouter, Link, Switch } from 'react-router-dom'
 import NowTime from './components/NowTime';
 import SearchFriend from './components/SearchFriend';
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <NowTime />
+      <ul>
+        <li>
+          <Link to='/profile/조도연'>조도연</Link>
+        </li>
+        <li>
+          <Link to='/profile/김철진'>김철진</Link>
+        </li>
+        <li>
+          <Link to='/profile/문재훈'>조도연</Link>
+        </li>
+        <li>
+          <Link to='/profile/전진영'>조도연</Link>
+        </li>
+        <li>
+          <Link to='/profile/오정민'>조도연</Link>
+        </li>
+        <li>
+          <Link to='/profile/김준석'>조도연</Link>  
+        </li>
+        <li>
+          <Link to='/profile/성민승'>조도연</Link>
+        </li>
+      </ul>
       <hr/>
-      <SearchFriend/>
-    </div>
+      <Switch>
+        <Route path='/' exact={true} component={NowTime}/>
+        <Route path='/profile/:username' component={SearchFriend} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/components/SearchFriend.tsx
+++ src/components/SearchFriend.tsx
@@ -1,4 +1,9 @@
 import React,{ useState } from 'react';
+import { RouteComponentProps } from 'react-router-dom'
+
+interface MatchParmas {
+  username:string;
+}
 
 const friends: { [key: string]: any } = {
   조도연: {
@@ -31,33 +36,15 @@ const friends: { [key: string]: any } = {
   }
 }
 
-const SearchFriend = () => {
-  const [inputText, setInputText] = useState('');
-
-  const [profile, setProfile] = useState({
-    nickname: '',
-    age: ''
-  });
-  const inputName = (e: React.ChangeEvent<HTMLInputElement>) => setInputText(e.target.value);
-  const noName = {
-    nickname: '찾을 수 없습니다.',
-    age: '찾을 수 없습니다.'
-  }
-  const searchName = () => {
-    const friend = friends[inputText];
-    if (!friend) {
-      setProfile(noName);
-    } else {
-      setProfile(friend);
-    }
-  }
+const SearchFriend = ({ match }:RouteComponentProps<MatchParmas>) => {
+  const { username } = match.params;
+  const profile = friends[username];
+  if (!profile) return <div>존재하지 않는 유저 입니다.</div>
+  
   return (
     <div>
-      <input value={inputText} onChange={inputName} name="name"/>
-      <button onClick={searchName}>출력</button>
       <div>
-        <p>카카오톡 7명 그룹채팅방 친구 이름을 입력해보세요.</p>
-        <p>이름 : {inputText}</p>
+        <p>이름 : {username}</p>
         <p>별명 : {profile.nickname}</p>
         <p>나이 : {profile.age}</p>
       </div>
