6cb8df7d8385ba93088bcbbff3fe7e8a2136e2c2
doyeon <jdy0210@naver.com>
2021-04-02 11:19:20 +0900

    remark inputfriend

--- src/components/SearchFriend.tsx
+++ src/components/SearchFriend.tsx
@@ -18,6 +18,7 @@ const SearchFriend = () => {
   },[])
   
   return (
+    // inputFriend컴포넌트를 나눈 이유는 렌더링 될때마다 getFriendList를 호출하는것이 성능상 불리하기 때문이고 그럴 필요도 없다.
     <InputFriend friendList={friendList} />
   );
 }
